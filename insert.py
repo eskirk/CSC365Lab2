135a00edd6627309bc79a283ef6e6c3ae9f02a1a
Elliot Kirk <elliot.s.kirk@gmail.com>
2018-01-28 15:56:37 -0800

    Consolidated repetitive code

--- insert.py
+++ insert.py
@@ -1,36 +1,35 @@
 import csv
 
+
+# Create an insert statement based on `insert` and write to a file based on `outputFile`
+def create_insert_statement(csvFile, outputFile, insert):
+   next(csvFile)
+   for row in csvFile:
+      values = map((lambda x: x), row)
+      statement = insert + '(' + ", ".join(values) + ');\n'
+      print(statement)
+      outputFile.write(statement)
+
+
 ##### INN #####
 
 ### Populate the 'Reservations' table
 file = open('./INN/Reservations.csv')
-output = open('./INN-populate.sql', 'w')
+outputFile = open('./INN-populate.sql', 'w')
 csvFile = csv.reader(file)
 headers = ['code', 'room', 'checkIn', 'checkOut', 'rate', 'lastName', 'firstName', 'adults', 'kids']
 insert = 'INSERT INTO Reservations (' + ', '.join(headers) + ') VALUES '
 
-# skip the header line
-next(csvFile)
-for row in csvFile:
-   values = map((lambda x: x), row)
-   statement = insert + '(' + ", ".join(values) + ');\n'
-   print(statement)
-   output.write(statement)
+create_insert_statement(csvFile, outputFile, insert)
 
 ### Populate the 'Rooms' table
 file = open('./INN/Rooms.csv')
-output = open('./INN-populate.sql', 'a')
+outputFile = open('./INN-populate.sql', 'a')
 csvFile = csv.reader(file)
 headers = ['roomId', 'roomName', 'beds', 'bedType', 'maxOccupancy', 'basePrice', 'decor'] 
 insert = 'INSERT INTO Rooms (' + ', '.join(headers) + ') VALUES '
 
-# skip the header line
-next(csvFile)
-for row in csvFile:
-   values = map((lambda x: x), row)
-   statement = insert + '(' + ", ".join(values) + ');\n'
-   print(statement)
-   output.write(statement)
+create_insert_statement(csvFile, outputFile, insert)
 
 
 
@@ -38,63 +37,39 @@ for row in csvFile:
 
 ### Populate the 'Customers' table
 file = open('./BAKERY/Customers.csv')
-output = open('./BAKERY-populate.sql', 'w')
+outputFile = open('./BAKERY-populate.sql', 'w')
 csvFile = csv.reader(file)
 headers = ['id', 'lastName', 'firstName']
 insert = 'INSERT INTO Customers (' + ', '.join(headers) + ') VALUES '
 
-# skip the header line
-next(csvFile)
-for row in csvFile:
-   values = map((lambda x: x), row)
-   statement = insert + '(' + ", ".join(values) + ');\n'
-   print(statement)
-   output.write(statement)
+create_insert_statement(csvFile, outputFile, insert)
 
 ### Populate the 'Goods' table
 file = open('./BAKERY/Goods.csv')
-output = open('./BAKERY-populate.sql', 'a')
+outputFile = open('./BAKERY-populate.sql', 'a')
 csvFile = csv.reader(file)
 headers = ['id', 'flavor', 'food', 'price']
 insert = 'INSERT INTO Goods (' + ', '.join(headers) + ') VALUES '
 
-# skip the header line
-next(csvFile)
-for row in csvFile:
-   values = map((lambda x: x), row)
-   statement = insert + '(' + ", ".join(values) + ');\n'
-   print(statement)
-   output.write(statement)
+create_insert_statement(csvFile, outputFile, insert)
 
 ### Populate the 'Items' table
 file = open('./BAKERY/Items.csv')
-output = open('./BAKERY-populate.sql', 'a')
+outputFile = open('./BAKERY-populate.sql', 'a')
 csvFile = csv.reader(file)
 headers = ['receipt', 'ordinal', 'item']
 insert = 'INSERT INTO Items (' + ', '.join(headers) + ') VALUES '
 
-# skip the header line
-next(csvFile)
-for row in csvFile:
-   values = map((lambda x: x), row)
-   statement = insert + '(' + ", ".join(values) + ');\n'
-   print(statement)
-   output.write(statement)
+create_insert_statement(csvFile, outputFile, insert)
 
 ### Populate the 'Receipts' table
 file = open('./BAKERY/receipts.csv')
-output = open('./BAKERY-populate.sql', 'a')
+outputFile = open('./BAKERY-populate.sql', 'a')
 csvFile = csv.reader(file)
 headers = ['receiptNum', 'soldDate', 'customerId']
 insert = 'INSERT INTO Receipts (' + ', '.join(headers) + ') VALUES '
 
-# skip the header line
-next(csvFile)
-for row in csvFile:
-   values = map((lambda x: x), row)
-   statement = insert + '(' + ", ".join(values) + ');\n'
-   print(statement)
-   output.write(statement)
+create_insert_statement(csvFile, outputFile, insert)
 
 
 
@@ -102,45 +77,27 @@ for row in csvFile:
 
 ### Populate the 'Airlines' table
 file = open('./AIRLINES/airlines.csv')
-output = open('./AIRLINES-populate.sql', 'w')
+outputFile = open('./AIRLINES-populate.sql', 'w')
 csvFile = csv.reader(file)
 headers = ['id', 'airline', 'abbreviation', 'country']
 insert = 'INSERT INTO Airlines (' + ', '.join(headers) + ') VALUES '
 
-# skip the header line
-next(csvFile)
-for row in csvFile:
-   values = map((lambda x: x), row)
-   statement = insert + '(' + ", ".join(values) + ');\n'
-   print(statement)
-   output.write(statement)
+create_insert_statement(csvFile, outputFile, insert)
 
 ### Populate the 'Airports' table
 file = open('./AIRLINES/airports100.csv')
-output = open('./AIRLINES-populate.sql', 'a')
+outputFile = open('./AIRLINES-populate.sql', 'a')
 csvFile = csv.reader(file)
 headers = ['city', 'airportCode', 'airportName', 'country', 'countryAbbrev']
 insert = 'INSERT INTO Airports (' + ', '.join(headers) + ') VALUES '
 
-# skip the header line
-next(csvFile)
-for row in csvFile:
-   values = map((lambda x: x), row)
-   statement = insert + '(' + ", ".join(values) + ');\n'
-   print(statement)
-   output.write(statement)
+create_insert_statement(csvFile, outputFile, insert)
 
 ### Populate the 'Flights' table
 file = open('./AIRLINES/flights.csv')
-output = open('./AIRLINES-populate.sql', 'a')
+outputFile = open('./AIRLINES-populate.sql', 'a')
 csvFile = csv.reader(file)
 headers = ['airline', 'flightNo', 'sourceAirport', 'destAirport']
 insert = 'INSERT INTO Flights (' + ', '.join(headers) + ') VALUES '
 
-# skip the header line
-next(csvFile)
-for row in csvFile:
-   values = map((lambda x: x), row)
-   statement = insert + '(' + ", ".join(values) + ');\n'
-   print(statement)
-   output.write(statement)
+create_insert_statement(csvFile, outputFile, insert)
